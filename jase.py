a57832b5e5d3100063d7bdb9dbad8e85d2eade6c
fabricioOkamoto <127327207+fabricioOkamoto@users.noreply.github.com>
2023-11-09 23:47:41 -0300

    Update jase.py

--- jase.py
+++ jase.py
@@ -1,11 +1,1 @@
-import os
-import openai
 
-openai.api_key = os.getenv('OPENAI_API_KEY')
-
-response = openai.ChatCompletion.create(
-  model="gpt-3.5",
-  messages=[],
-  temperature=0,
-  max_tokens=1024
-)
